441431587b48b5bee6e48afe1c7028957c048619
Ege Hatırnaz <egehatirnaz@hotmail.com>
2019-08-19 16:10:11 +0300

    Checking for fixed analysis_errors.

--- analysis.py
+++ analysis.py
@@ -36,44 +36,79 @@ class Analyser:
             # Check for missing attributes. If problems are not noted before, add them.
             # Remove any problems that are solved.
 
-            # Spotted problems that are not noted before:
-            if meta_title is None and not self.db_obj.exists(
-                        'analysis_errors', [('url_id', checked_id), ('error_id', 2)]):
+            # Get the problems related to that url id.
+            url_errors = [row['error_id'] for row in
+                          self.db_obj.execute("SELECT error_id FROM analysis_errors WHERE url_id = " + str(checked_id))]
+            print(url_errors)
+
+            # Title
+            if meta_title is None and 2 not in url_errors:
                 print("Title is missing and this is not noted before!")
                 self.db_obj.insert_data('analysis_errors',
                                         [(checked_id, 2)],
                                         COLUMNS=['url_id', 'error_id'])
+            elif meta_title is not None and 2 in url_errors:
+                print("Title was missing but it seems to be fixed now.")
+                try:
+                    self.db_obj.execute(
+                        "DELETE FROM analysis_errors WHERE url_id = {0} AND error_id = {1} LIMIT 1"
+                            .format(checked_id, 2))
+                except Exception as e:
+                    print(e)
 
-            if meta_desc is None and not self.db_obj.exists(
-                        'analysis_errors', [('url_id', checked_id), ('error_id', 1)]):
+            # Meta Desc
+            if meta_desc is None and 1 not in url_errors:
                 print("Desc is missing and this is not noted before!")
                 self.db_obj.insert_data('analysis_errors',
                                         [(checked_id, 1)],
                                         COLUMNS=['url_id', 'error_id'])
+            elif meta_desc is not None and 1 in url_errors:
+                print("Desc was missing but it seems to be fixed now.")
+                try:
+                    self.db_obj.execute(
+                        "DELETE FROM analysis_errors WHERE url_id = {0} AND error_id = {1} LIMIT 1"
+                            .format(checked_id, 1))
+                except Exception as e:
+                    print(e)
 
-            if h1 is None and not self.db_obj.exists(
-                        'analysis_errors', [('url_id', checked_id), ('error_id', 3)]):
+            # H1
+            if h1 is None and 3 not in url_errors:
                 print("H1 is missing and this is not noted before!")
-                self.db_obj.insert_data('analysis_errors',
-                                        [(checked_id, 3)],
-                                        COLUMNS=['url_id', 'error_id'])
+                try:
+                    self.db_obj.insert_data('analysis_errors',
+                                            [(checked_id, 3)],
+                                            COLUMNS=['url_id', 'error_id'])
+                except Exception as e:
+                    print(e)
+            elif h1 is not None and 3 in url_errors:
+                print("H1 was missing but it seems to be fixed now.")
+                try:
+                    self.db_obj.execute(
+                        "DELETE FROM analysis_errors WHERE url_id = {0} AND error_id = {1} LIMIT 1".format(checked_id,
+                                                                                                           3))
+                except Exception as e:
+                    print(e)
 
-            if h2 is None and not self.db_obj.exists(
-                        'analysis_errors', [('url_id', checked_id), ('error_id', 4)]):
+            # H2
+            if h2 is None and 4 not in url_errors:
                 print("H2 is missing and this is not noted before!")
                 self.db_obj.insert_data('analysis_errors',
                                         [(checked_id, 4)],
                                         COLUMNS=['url_id', 'error_id'])
+            elif h2 is not None and 4 in url_errors:
+                print("H2 was missing but it seems to be fixed now.")
+                self.db_obj.execute(
+                    "DELETE FROM analysis_errors WHERE url_id = {0} AND error_id = {1} LIMIT 1".format(checked_id, 4))
 
-            # Check for duplicate attributes.
+            # TODO: Check for duplicate attributes.
 
             # Update the crawled website info.
             self.db_obj.update_data('websites', [
-                                        ('meta_title', meta_title),
-                                        ('meta_desc', meta_desc),
-                                        ('h1', h1),
-                                        ('h2', h2)
-                                    ], checked_id)
+                ('meta_title', meta_title),
+                ('meta_desc', meta_desc),
+                ('h1', h1),
+                ('h2', h2)
+            ], checked_id)
 
         else:  # A unique record.
             try:
@@ -83,26 +118,26 @@ class Analyser:
                 insert_id = self.db_obj.get_last_insert_id()
 
                 # Check for missing attributes.
-                if meta_title is None and not self.db_obj.exists(
-                        'analysis_errors', [('url_id', insert_id), ('error_id', 2)]):
+                if meta_title is None:
+                    print("Title is not found!")
                     self.db_obj.insert_data('analysis_errors',
                                             [(insert_id, 2)],
                                             COLUMNS=['url_id', 'error_id'])
 
-                if meta_desc is None and not self.db_obj.exists(
-                        'analysis_errors', [('url_id', insert_id), ('error_id', 1)]):
+                if meta_desc is None:
+                    print("Meta Desc is not found!")
                     self.db_obj.insert_data('analysis_errors',
                                             [(insert_id, 1)],
                                             COLUMNS=['url_id', 'error_id'])
 
-                if h1 is None and not self.db_obj.exists(
-                        'analysis_errors', [('url_id', insert_id), ('error_id', 3)]):
+                if h1 is None:
+                    print("H1 is not found!")
                     self.db_obj.insert_data('analysis_errors',
                                             [(insert_id, 3)],
                                             COLUMNS=['url_id', 'error_id'])
 
-                if h2 is None and not self.db_obj.exists(
-                        'analysis_errors', [('url_id', insert_id), ('error_id', 4)]):
+                if h2 is None:
+                    print("H2 is not found!")
                     self.db_obj.insert_data('analysis_errors',
                                             [(insert_id, 4)],
                                             COLUMNS=['url_id', 'error_id'])
@@ -119,7 +154,7 @@ class Analyser:
                                     COLUMNS=['url_id', 'meta_title', 'meta_desc', 'h1', 'h2'])
 
     def main(self):
-        self.analyse("https://www.icerik.com/urunler/test2")
+        self.analyse("https://www.jotform.com/theme-store/collection/3d")
 
 
 if __name__ == '__main__':
